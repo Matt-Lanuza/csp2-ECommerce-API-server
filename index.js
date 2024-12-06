ea6c212b57ff34478579cb1ce217a5e6a326a794
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-06 21:15:03 +0800

    update cors

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const corsOptions = {
     origin: [
         'http://localhost:3000',
         'https://csp2-ecommerce-api-server.onrender.com',
-        'http://csp3-e-commerce-client-ni3s.vercel.app'
+        'https://csp3-e-commerce-client.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
